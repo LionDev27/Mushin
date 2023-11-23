649b4711c51940d05f0fe90306dcbc15798b7aaf
León Zhou <leonzhou2b@gmail.com>
2023-11-23 11:15:03 +0100

    refactor: Recollectables Acceleration

--- Assets/Mushin/Scripts/Gameplay/Recollectable.cs
+++ Assets/Mushin/Scripts/Gameplay/Recollectable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Recollectable : MonoBehaviour, IPoolable
 {
@@ -10,31 +11,38 @@ public class Recollectable : MonoBehaviour, IPoolable
     private Transform _target;
     private bool _hasTarget;
     private string _poolTag;
+    private float _timer;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (!_hasTarget) return;
+        _timer += Time.deltaTime;
+    }
+
     private void FixedUpdate()
     {
-        if (_hasTarget)
-        {
-            Vector2 targetDir = _target.position - transform.position;
-            targetDir.Normalize();
-            _rb.velocity = targetDir * _moveSpeed * Time.fixedDeltaTime * 100;
-        }
+        if (!_hasTarget) return;
+        Vector2 targetDir = _target.position - transform.position;
+        targetDir.Normalize();
+        _rb.velocity = targetDir * _moveSpeed * Time.fixedDeltaTime * 100 * _timer;
     }
 
     public void SetTarget(Transform target)
     {
         _target = target;
+        _timer = 1f;
         _hasTarget = true;
     }
 
     public void RemoveTarget()
     {
         if (!_hasTarget) return;
+        _timer = 0f;
         _hasTarget = false;
         _target = null;
     }
